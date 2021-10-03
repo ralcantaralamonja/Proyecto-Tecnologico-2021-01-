55ebfacd8b16160a6b6ee7c4fb89f4c5dbd7a1ab
Norvil Arce <norvil.arce@gmail.com>
2021-10-03 18:56:28 -0500

    fix

--- Backend/src/main/java/pe/isil/controller/HuespedController.java
+++ Backend/src/main/java/pe/isil/controller/HuespedController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/huespedes")
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class HuespedController {
 
     //METODOS CON PERMISOS DE ADMIN
